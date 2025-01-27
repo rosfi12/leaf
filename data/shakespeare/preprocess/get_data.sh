e595ab89f03153a0cf36df363b4f6ecff7f10d66
Rosamaria Fiata <s329502@studenti.polito.it>
2025-01-27 15:46:17 +0100

    leaf

--- data/shakespeare/preprocess/get_data.sh
+++ data/shakespeare/preprocess/get_data.sh
@@ -7,8 +7,13 @@ cd ../data/raw_data
 # rm 1994-01-100.zip
 # mv 100.txt raw_data.txt
 
-wget --adjust-extension http://www.gutenberg.org/files/100/100-0.txt
-mv 100-0.txt raw_data.txt
+wget http://www.gutenberg.org/files/100/old/old/1994-01-100.zip
+unzip 1994-01-100.zip
+rm 1994-01-100.zip
+mv 100.txt raw_data.txt
+
+# wget --adjust-extension http://www.gutenberg.org/files/100/100-0.txt
+# mv 100-0.txt raw_data.txt
 
 # wget --adjust-extension https://www.gutenberg.org/cache/epub/100/pg100.txt
 # mv pg100.txt raw_data.txt
